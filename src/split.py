00caa67db7c7491664491207b02b87dfedd8856d
Niwo1403 <nicolai-wolfrom@gmx.de>
2024-01-08 17:06:24 +0100

    Refactored split.py, added argument parsing, and added configurable output_folder_path.

--- src/split.py
+++ src/split.py
@@ -7,26 +7,54 @@ The datasets get random shuffled before splitting.
 import shutil
 import random
 import glob
-import sys, os
+import sys
+import os
 import numpy as np
 from tqdm import tqdm
+from argparse import ArgumentParser
+from typing import List
+
+
+# Parse arguments
+parser = ArgumentParser(
+    prog='split.py',
+    description='Splits dataset into required format.'
+)
+parser.add_argument("--pic_folder_path", help="The root-folder containing the required subdirectories",
+                    default="./../data/preprocessed", required=False)
+parser.add_argument("--output_folder_path", help="The output path to write the images to",
+                    default=None, required=False)
+parser.add_argument("--class_dirs_to_use", help="Classes/subdirectories to include in the data sets",
+                    default=["images", "masks"], nargs='*', required=False)
+parser.add_argument("--train_ratio", help="Ratio of data to be used for training between 0 and 1",
+                    default=0.8, required=False)  # .7
+parser.add_argument("--test_ratio", help="Ratio of data to be used for testing between 0 and 1",
+                    default=0.2, required=False)  # .3
+parser.add_argument("--val_ratio", help="Ratio of data to be used for validating between 0 and 1",
+                    default=0.0, required=False)
+args, _ = parser.parse_known_args()  # Ignore unexpected arguments
+
 
 # the root-folder containing the subdirectories/classes/resolutions
 #   We need to adapt this to an output path as well and make it a parameter 
-pic_folder_path = "./../data/preprocessed" 
+pic_folder_path = args.pic_folder_path
 
 # classes/subdirectories to include in the data sets. 
 #   We have only one class for segmentation
 #   But we can use this for classification as well when we have 6 classes
 #   Therefore, this script needs some adjustments
-class_dirs_to_use = ["images", "masks"]
+class_dirs_to_use = args.class_dirs_to_use
+
+# output path to write the images to
+output_folder_path = args.output_folder_path or pic_folder_path
 
 # set some ratios:
-train_ratio = 0.80 #.7
-test_ratio = 0.20  #.3
-val_ratio = 0.0
+train_ratio = args.train_ratio
+test_ratio = args.test_ratio
+val_ratio = args.val_ratio
 
 # TODO: convert ^ to sys.argv parameters, adjust some things, test it
+
 
 # Global parameters for the current session:
 # (reproducability)
@@ -36,24 +64,28 @@ def set_seed(seed=1129142083):
     os.environ['PYTHONHASHSEED'] = str(seed)
     return
 
+
 # Disable print
 def block_print():
     sys.stdout = open(os.devnull, 'w')
+
 
 # Restore print
 def enable_print():
     sys.stdout = sys.__stdout__
 
+
+def copy_files_to_subdir(filenames: List[str], subdir: str) -> None:
+    for filename in filenames:
+        shutil.copy(filename, output_folder_path + '/' + subdir + cls)
+
+
 for cls in tqdm(class_dirs_to_use, desc = "Copy train/test/val sets"):
     #block_print()
-    if not os.path.exists(pic_folder_path +'/train' + cls):
-        os.makedirs(pic_folder_path +'/train' + cls)
-    if not os.path.exists(pic_folder_path + '/val' + cls):
-        os.makedirs(pic_folder_path +'/val' + cls)
-    if not os.path.exists(pic_folder_path + '/test' + cls):
-        os.makedirs(pic_folder_path +'/test' + cls)
+    for sub_dir in ['train', 'test', 'val']:
+        os.makedirs(output_folder_path + '/' + sub_dir + cls, exist_ok=True)
     # Creating partitions of the data after shuffeling
-    src = pic_folder_path + cls # Folder to copy images from
+    src = pic_folder_path + cls  # Folder to copy images from
 
     allFileNames = glob.glob(src+"/*.tiff")
     
@@ -65,19 +97,10 @@ for cls in tqdm(class_dirs_to_use, desc = "Copy train/test/val sets"):
                                                               [int(len(allFileNames) * (1 - (val_ratio + test_ratio))), 
                                                                int(len(allFileNames) * (1 - test_ratio))])
 
-    train_FileNames = [name for name in train_FileNames.tolist()]
-    val_FileNames = [name for name in val_FileNames.tolist()]
-    test_FileNames = [name for name in test_FileNames.tolist()]
-
     # Copy-pasting images
-    for name in train_FileNames:
-        shutil.copy(name, pic_folder_path +'/train' + cls)
-
-    for name in test_FileNames:
-        shutil.copy(name, pic_folder_path +'/test' + cls)
-
-    for name in val_FileNames:
-        shutil.copy(name, pic_folder_path +'/val' + cls)
+    copy_files_to_subdir(train_FileNames.tolist(), "train")
+    copy_files_to_subdir(test_FileNames.tolist(), "test")
+    copy_files_to_subdir(val_FileNames.tolist(), "val")
 
 #enable_print()
         
