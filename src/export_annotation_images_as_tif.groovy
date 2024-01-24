aa6affc9d120c91f0ce2f6ad4ad52b5aaa360550
Kevin <kev.zid@gmx.de>
2024-01-24 11:54:42 +0100

    Add Groovy Script to save all annotations of a Qupath project automatically as tif

---
+++ src/export_annotation_images_as_tif.groovy
@@ -0,0 +1,22 @@
+// Usage:
+// Load the script as user script in Qupath for a given project
+// Line 10: Enter corresponding Qupath Project Base Directory
+// Open Project in Qupath, select first Image and run the script by pressing: Run -> Run for Project
+
+import qupath.lib.images.ImageData
+
+def server = getCurrentServer()
+
+def baseImageName = getProjectEntry().getImageName()
+
+path = buildFilePath("/home/kikou/Uni/organoid/annotation", 'ROIsTif')
+mkdirs(path)
+
+getAnnotationObjects().eachWithIndex{it, x->
+        println("Working on: "+it)
+	def roi = it.getROI()
+	def requestROI = RegionRequest.createInstance(server.getPath(), 1, roi)
+	currentImagePath = buildFilePath(PROJECT_BASE_DIR, 'ROIsTif',baseImageName +it+'_'+ x + '.tif')
+	writeImageRegion(server, requestROI, currentImagePath)
+}
+print 'Done!'
