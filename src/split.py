8cfa0994684e7dafcba5a4224e21752f2abb3119
Scaramir <maxotto45@gmail.com>
2024-05-15 21:59:39 +0200

    chore: Update paths and formatting in split.py

--- src/split.py
+++ src/split.py
@@ -1,8 +1,8 @@
-'''
+"""
 Maximilian Otto, 2022, @scaramir
 Split the data of different classes/folders into test, train and validation folders with the desired ratio
 The datasets get random shuffled before splitting.   
-'''
+"""
 
 import shutil
 import random
@@ -17,29 +17,53 @@ from typing import List
 
 # Parse arguments
 parser = ArgumentParser(
-    prog='split.py',
-    description='Splits dataset into required format.'
+    prog="split.py", description="Splits dataset into required format."
 )
-parser.add_argument("--pic_folder_path", help="The root-folder containing the required subdirectories",
-                    default="./../data/data_sets/classification/objects", required=False)
-parser.add_argument("--output_folder_path", help="The output path to write the images to",
-                    default="./../data/data_sets/classification", required=False)
-parser.add_argument("--class_dirs_to_use", help="Folder names to include in the data sets (classes or subdirectories to split into train/test/val sets)",
-                    default=["Branched", "Burned out", "Cyst", "early Branching", "Filled in", "Ring"], nargs='*', required=False)
-parser.add_argument("--train_ratio", help="Ratio of data to be used for training between 0 and 1",
-                    default=0.8, required=False)  # .7
-parser.add_argument("--test_ratio", help="Ratio of data to be used for testing between 0 and 1",
-                    default=0.2, required=False)  # .3
-parser.add_argument("--val_ratio", help="Ratio of data to be used for validating between 0 and 1",
-                    default=0.0, required=False)  # stardist expects the test images to be in a folder called `val`, so set this to 0.2 instead
+parser.add_argument(
+    "--pic_folder_path",
+    help="The root-folder containing the required subdirectories",
+    default="./../data/data_sets/classification/big_data_set/objects",
+    required=False,
+)
+parser.add_argument(
+    "--output_folder_path",
+    help="The output path to write the images to",
+    default="./../data/data_sets/classification/big_data/set",
+    required=False,
+)
+parser.add_argument(
+    "--class_dirs_to_use",
+    help="Folder names to include in the data sets (classes or subdirectories to split into train/test/val sets)",
+    default=["Branched", "Burned out", "Cyst", "Early Branching", "Filled in", "Ring"],
+    nargs="*",
+    required=False,
+)
+parser.add_argument(
+    "--train_ratio",
+    help="Ratio of data to be used for training between 0 and 1",
+    default=0.8,
+    required=False,
+)  # .7
+parser.add_argument(
+    "--test_ratio",
+    help="Ratio of data to be used for testing between 0 and 1",
+    default=0.2,
+    required=False,
+)  # .3
+parser.add_argument(
+    "--val_ratio",
+    help="Ratio of data to be used for validating between 0 and 1",
+    default=0.0,
+    required=False,
+)  # stardist expects the test images to be in a folder called `val`, so set this to 0.2 instead
 args, _ = parser.parse_known_args()  # Ignore unexpected arguments
 
 
 # the root-folder containing the subdirectories/classes/resolutions
-#   We need to adapt this to an output path as well and make it a parameter 
+#   We need to adapt this to an output path as well and make it a parameter
 pic_folder_path = args.pic_folder_path
 
-# classes/subdirectories to include in the data sets. 
+# classes/subdirectories to include in the data sets.
 #   We have only one class for segmentation
 #   But we can use this for classification as well when we have 6 classes
 #   Therefore, this script needs some adjustments
@@ -52,8 +76,7 @@ if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_path)
 
 if not os.path.exists(pic_folder_path):
-    raise FileNotFoundError(
-        f"Path to raw images does not exist: {pic_folder_path}")
+    raise FileNotFoundError(f"Path to raw images does not exist: {pic_folder_path}")
 
 # set some ratios:
 train_ratio = args.train_ratio
@@ -66,13 +89,13 @@ val_ratio = args.val_ratio
 def set_seed(seed=1129142083):
     np.random.seed(seed)
     random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
     return
 
 
 # Disable print
 def block_print():
-    sys.stdout = open(os.devnull, 'w')
+    sys.stdout = open(os.devnull, "w")
 
 # Restore print
 def enable_print():
@@ -81,31 +104,35 @@ def enable_print():
 
 def copy_files_to_subdir(filenames: List[str], subdir: str) -> None:
     for filename in filenames:
-        shutil.copy(filename, output_folder_path + '/' + subdir + '/' + cls)
+        shutil.copy(filename, output_folder_path + "/" + subdir + "/" + cls)
 
 
-for cls in tqdm(class_dirs_to_use, desc = "Copy train/test/val sets"):
-    #block_print()
-    for sub_dir in ['train', 'test', 'val']:
-        os.makedirs(output_folder_path + '/' + sub_dir + '/' + cls, exist_ok=True)
+for cls in tqdm(class_dirs_to_use, desc="Copy train/test/val sets"):
+    # block_print()
+    for sub_dir in ["train", "test", "val"]:
+        os.makedirs(output_folder_path + "/" + sub_dir + "/" + cls, exist_ok=True)
     # Creating partitions of the data after shuffeling
-    src = pic_folder_path + '/' + cls  # Folder to copy images from
+    src = pic_folder_path + "/" + cls  # Folder to copy images from
 
-    allFileNames = glob.glob(src+"/*.tiff*")
-    
+    allFileNames = glob.glob(src + "/*.tiff*")
+
     # set seed so you get the same splits of every folder and therefore matching images and masks
     set_seed()
 
     np.random.shuffle(allFileNames)
-    train_FileNames, val_FileNames, test_FileNames = np.split(np.array(allFileNames),
-                                                              [int(len(allFileNames) * (1 - (val_ratio + test_ratio))), 
-                                                               int(len(allFileNames) * (1 - test_ratio))])
+    train_FileNames, val_FileNames, test_FileNames = np.split(
+        np.array(allFileNames),
+        [
+            int(len(allFileNames) * (1 - (val_ratio + test_ratio))),
+            int(len(allFileNames) * (1 - test_ratio)),
+        ],
+    )
 
     # Copy-pasting images
     copy_files_to_subdir(train_FileNames.tolist(), "train")
     copy_files_to_subdir(test_FileNames.tolist(), "test")
     copy_files_to_subdir(val_FileNames.tolist(), "val")
 
-#enable_print()
+# enable_print()
 
 print("Done splitting data into train/test/val sets!")
