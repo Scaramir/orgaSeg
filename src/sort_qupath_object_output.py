a457cef8fb21298f82a4df166413a59d5aba7512
Scaramir <maxotto45@gmail.com>
2024-05-15 21:58:09 +0200

    Update default paths in sort_qupath_object_output.py + formatting

--- src/sort_qupath_object_output.py
+++ src/sort_qupath_object_output.py
@@ -7,37 +7,50 @@ from pathlib import Path
 import argparse
 from tqdm import tqdm
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Sorts QuPath object output into folders by class. The class has to be denoted in the file name in brackets. Every file has to have brackets with a annotation/class name in it.')
-    parser.add_argument('--root', type=str, help='Path to root directory',
-                        default='./../../annotation - organoid pictures/export/ROIsTif')
-    parser.add_argument('--target_folder', type=str, help='Path to target folder',
-                        default= './../data/data_sets/classification/objects')
+    parser = argparse.ArgumentParser(
+        description="Sorts QuPath object output into folders by class. The class has to be denoted in the file name in brackets. Every file has to have brackets with a annotation/class name in it."
+    )
+    parser.add_argument(
+        "--root",
+        type=str,
+        help="Path to root directory",
+        default="S:/studium/beliks_exploiting_dl_seminar/data/Oranoids for AI-20240330T140242Z-002/Oranoids for AI/All 713 pictures Qupath annotated/export/ROIsTif",
+    )
+    parser.add_argument(
+        "--target_folder",
+        type=str,
+        help="Path to target folder",
+        default="./../data/data_sets/classification/big_data_set/objects",
+    )
     args, _ = parser.parse_known_args()
     return args
 
 
 def get_file_names(root: Path):
-    file_names = [name
-                  for name in root.glob('*.tif')
-                  if "(" in str(name)]
+    file_names = [name for name in root.glob("*.tif") if "(" in str(name)]
     return file_names
+
 
 # sort file names by whatever is written in '()'
 def sort_file_names(file_names):
     # TODO: List index out of range -> file names UPDATE: möglicher fix in get_file_names Funktion (exports ohne Klammer ignorieren)
-    sorted_file_names = sorted(file_names, key=lambda x: x.name.split('(')[1].split(')')[0])
+    sorted_file_names = sorted(
+        file_names, key=lambda x: x.name.split("(")[1].split(")")[0]
+    )
     return sorted_file_names
 
-def create_folders(target_folder: Path, sorted_file_names):
 
-    for file_name in tqdm(sorted_file_names, desc='Creating folders and sorting files'):
-        folder_name = file_name.name.split('(')[1].split(')')[0]
+def create_folders(target_folder: Path, sorted_file_names):
+    for file_name in tqdm(sorted_file_names, desc="Creating folders and sorting files"):
+        folder_name = file_name.name.split("(")[1].split(")")[0]
         folder = target_folder / folder_name
         folder.mkdir(exist_ok=True, parents=True)
         file_name.replace(folder / file_name.name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = get_args()
     root = Path(args.root).resolve()
     target_folder = Path(args.target_folder).resolve()
