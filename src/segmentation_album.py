fb674c09986d7f7429d62ceaa4b0ceddb950e5de
Scaramir <maxotto45@gmail.com>
2024-05-20 22:19:49 +0200

    segmentation settings for all models (:

--- src/segmentation_album.py
+++ src/segmentation_album.py
@@ -1,8 +1,4 @@
-import os, sys
-from pathlib import Path
-from typing import List
-import numpy as np
-from tqdm import tqdm
+import os
 import subprocess
 
 # check if a conda environment called "album" exists
@@ -13,18 +9,22 @@ def install_album_catalog():
         os.system("conda activate album && album add-catalog https://gitlab.com/album-app/catalogs/image-challenges-dev.git")
         os.system("conda activate album && album install stardist_train && album install stardist_predict")
     return
-install_album_catalog()
+# install_album_catalog()
 
 def stardist_train():
+    import subprocess
     command = (
         "conda activate album && "
         "album run stardist_train "
-        "--root S:/studium/ORGAnoids_Applied_DL_FU/data/data_sets/stardist "
-        "--out S:/studium/ORGAnoids_Applied_DL_FU/data/data_sets/stardist_out "
-        "--epochs 50 "
-        "--steps_per_epoch 50 "
+        "--root S:/studium/ORGAnoids_Applied_DL_FU/data/data_sets/stardist/first_data_set "
+        "--out S:/studium/ORGAnoids_Applied_DL_FU/data/data_sets/stardist_out/first_data_set "
+        "--epochs 100 "
+        "--steps_per_epoch 200 "  # num_samples / batch_size = steps_per_epoch (first_data_set: 50 steps aka batch_size=5)
         "--mode 2D "
-        "--train_patch_size 944,944"
+        "--train_batch_size 4 "
+        "--train_patch_size 176,176 "  # roughly a quarter of our down-sampled images
+        "--total_memory 24000 "
+        # "--train_sample_cache False " # default is True
     )
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     # Capture the live output
@@ -58,3 +58,15 @@ def stardist_predict():
 # TODO: Discuss the usage of Cellpose in our scenario (https://www.sciencedirect.com/science/article/pii/S2667290122000420)
 
 
+# NOTE: ALL images containing "oaf" are in the wrong resolution, are duplicates and we shoudl just ignore them
+def check_shapes_of_images_in_folder(folder_path):
+    import os
+    from PIL import Image
+    shapes = []
+    for file in os.listdir(folder_path):
+        if file.endswith(".tif"):
+            img = Image.open(os.path.join(folder_path, file))
+            shapes.append(img.size)
+            # if img.size != (504, 380):
+                # print(file)
+    return list(set(shapes))
